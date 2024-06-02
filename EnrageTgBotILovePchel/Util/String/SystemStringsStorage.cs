57acae3948a2fda15b1a1a8b6f944abeb30b34e6
Volodia5 <vladimirrogozin007@gmail.com>
2024-06-02 18:12:31 +0300

    asd
    update code

--- EnrageTgBotILovePchel/Util/String/SystemStringsStorage.cs
+++ EnrageTgBotILovePchel/Util/String/SystemStringsStorage.cs
@@ -4,5 +4,5 @@ public class SystemStringsStorage
 {
     public const string CommandStart = "/start";
     public const string CommandReset = "/reset";
-    public const string TelegramToken = "7044909397:AAHgcHfxO6kZ_IQqrnF7gChMFp5y66ePGOM";
+    public const string TelegramToken = "here is your th bot token";
 }
